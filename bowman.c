b247ffa1ddc10f6cba4e2694c73ad4da8d5d2923
adrianjorge.sanchez <adrianjorge.sanchez@students.salle.url.edu>
2023-10-13 11:11:47 +0200

    Bowman Ctrl+C handled

--- bowman.c
+++ bowman.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+#define printF(x) write(1, x, strlen(x))
+
+typedef struct {
+    char* name;
+    char* path;
+    char* ip;
+    int port;
+} Config;
+
+Config config;
+
+void sig_handler(int sigsum) {
+
+    switch(sigsum) {
+        case SIGINT:
+            printF("Aborting\n");
+            free(config.name);
+            free(config.path);
+            free(config.ip);
+            exit(0);
+            break;
+    }
+}
+
 int main() {
+
+    signal(SIGINT, sig_handler);
+    
 
     return 0;
 }
